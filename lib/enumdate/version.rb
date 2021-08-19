f2faaec11703d1d0c8a84a9af95101ee3d02a098
Yoshinari Nomura <nom@quickhack.net>
2021-08-19 11:18:54 +0900

    First import

---
+++ lib/enumdate/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Enumdate
+  VERSION = "0.1.0"
+end
---
+++ test/enumdate_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class EnumdateTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Enumdate::VERSION
+  end
+end
