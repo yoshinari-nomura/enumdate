1d8ab4cc9f80ff863fb997296b0b34d072e7e06e
Yoshinari Nomura <nom@quickhack.net>
2021-08-22 23:39:28 +0900

    Update interface

--- lib/enumdate.rb
+++ lib/enumdate.rb
@@ -13,4 +13,73 @@ module Enumdate
   autoload :DateFrame,      "#{dir}/date_frame.rb"
   autoload :DateHelper,     "#{dir}/date_helper.rb"
   autoload :VERSION,        "#{dir}/version.rb"
+
+  class << self
+    # @return [DateEnumerator::YearlyByMonthday]
+    def yearly_by_monthday(first_date, month: nil, mday: nil, interval: 1)
+      month ||= first_date.month
+      mday  ||= first_date.mday
+      DateEnumerator::YearlyByMonthday.new(
+        first_date: first_date,
+        month: month,
+        mday: mday,
+        interval: interval
+      )
+    end
+
+    # @return [DateEnumerator::YearlyByDay]
+    def yearly_by_day(first_date, month: nil, nth: nil, wday: nil, interval: 1)
+      month ||= first_date.month
+      nth   ||= (first_date.mday + 6) / 7
+      wday  ||= first_date.wday
+      DateEnumerator::YearlyByDay.new(
+        first_date: first_date,
+        month: month,
+        nth: nth,
+        wday: wday,
+        interval: interval
+      )
+    end
+
+    # @return [DateEnumerator::MonthlyByMonthday]
+    def monthly_by_monthday(first_date, mday: nil, interval: 1)
+      mday ||= first_date.mday
+      DateEnumerator::MonthlyByMonthday.new(
+        first_date: first_date,
+        mday: mday,
+        interval: interval
+      )
+    end
+
+    # @return [DateEnumerator::MonthlyByDay]
+    def monthly_by_day(first_date, nth: nil, wday: nil, interval: 1)
+      nth  ||= (first_date.mday + 6) / 7
+      wday ||= first_date.wday
+      DateEnumerator::MonthlyByDay.new(
+        first_date: first_date,
+        nth: nth,
+        wday: wday,
+        interval: interval
+      )
+    end
+
+    # @return [DateEnumerator::Weekly]
+    def weekly(first_date, wday: nil, wkst: 1, interval: 1)
+      wday ||= first_date.wday
+      DateEnumerator::Weekly.new(
+        first_date: first_date,
+        wday: wday,
+        wkst: wkst,
+        interval: interval
+      )
+    end
+
+    # @return [DateEnumerator::Daily]
+    def daily(first_date, interval: 1)
+      DateEnumerator::Daily.new(
+        first_date: first_date,
+        interval: interval
+      )
+    end
+  end
 end
